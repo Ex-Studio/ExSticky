4a0db05570d78b4d1d4b7ee6d9aa553174691d39
yxj <564197835@qq.com>
2022-06-04 20:02:12 +0800

    remove and save text selection

--- ExSticky/App/TextWindow/TextWindowDelegate.swift
+++ ExSticky/App/TextWindow/TextWindowDelegate.swift
@@ -2,12 +2,20 @@ import Cocoa
 import XCLog
 
 class TextWindowDelegate: NSObject, NSWindowDelegate {
+    // MARK: - solved: window resigns key and the selection becomes not transparent (only consider single selection)
+
+    private var previous_selection = NSRange(location: 0, length: 0)
+
     func windowDidResignKey(_ notification: Notification) {
         guard let window = notification.object as? TextWindow else { return }
-        XCLog(.debug, "\(window.view!.textView.selectedRanges)")
-        window.view!.textView.setSelectedRange(NSRange(
-            location: window.view!.textView.selectedRanges.first!.rangeValue.location, // save position of cursor
-            length: 0 // remove selection
-        ))
+//        XCLog(.debug, "\(window.view!.textView.selectedRanges)")
+        self.previous_selection = window.view!.textView.selectedRanges.first!.rangeValue
+        // remove selection
+        window.view!.textView.setSelectedRange(NSRange(location: 0, length: 0))
+    }
+
+    func windowDidBecomeKey(_ notification: Notification) {
+        guard let window = notification.object as? TextWindow else { return }
+        window.view!.textView.setSelectedRange(previous_selection)
     }
 }
